b7e00bb100e5bd7bfe5c33238e47c5ce7b93990b
Simo Fanna <simofanna93@gmail.com>
2017-08-05 12:34:05 +0200

    line 39 shorter solution

--- bowling.py
+++ bowling.py
@@ -30,14 +30,13 @@ def score(game):
             frame += 1
     return result
 
+
 def get_value(char):
     if char == '1' or char == '2' or char == '3' or \
        char == '4' or char == '5' or char == '6' or \
        char == '7' or char == '8' or char == '9':
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char == 'X' or char == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
