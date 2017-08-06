bc934dff6ed7b4b3a9a5233427eccb96bfeecfae
Simo Fanna <simofanna93@gmail.com>
2017-08-06 20:02:42 +0200

    delete last variable + 25 line fix

--- bowling.py
+++ bowling.py
@@ -5,25 +5,27 @@ def score(game):
 
     for roll in range(len(game)):
         if game[roll] == '/':
-            result += 10 - last
+            result += 10 - get_value(game[roll - 1])
         else:
-            result += get_value(game[roll])    
+            result += get_value(game[roll])
+         
         if frame < 10 and get_value(game[roll]) == 10:
             if game[roll] == '/':
                 result += get_value(game[roll+1])
+                
             elif game[roll] == 'X' or game[roll] == 'x':
                 result += get_value(game[roll+1])
                 if game[roll+2] == '/':
                     result += 10 - get_value(game[roll+1])
                 else:
                     result += get_value(game[roll+2])
-        last = get_value(game[roll])
-        if not in_first_half:
-            frame += 1
+
         if in_first_half:
             in_first_half = False
         else:
+            frame += 1
             in_first_half = True
+
         if game[roll].lower() == 'x':
             in_first_half = True
             frame += 1
